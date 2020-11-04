1427244b853eab441b6294f5f7127a928aa52b1f
annanwb <annaneibisono@gmail.com>
2020-11-04 08:52:41 +0700

    branch annan

---
+++ resources/views/about.blade.php
@@ -0,0 +1,67 @@
+@extends('template.master')
+@section('content')
+
+<div class="hero-2" style="background-image: url('images/mieayam2_res.jpg');">
+    <div class="container">
+       <div class="row justify-content-center text-center align-items-center">
+         <div class="col-md-8">
+           <h2>Tentang Kami</h2>
+         </div>
+       </div>
+     </div>
+   </div>
+
+   <div class="site-section py-5 custom-border-bottom" data-aos="fade">
+    <div class="container">
+      <div class="row mb-5">
+        <div class="col-md-6">
+          <div class="block-16">
+            <figure>
+              <img src="images/vector_mie.png" alt="Image placeholder" class="img-fluid">
+
+            </figure>
+          </div>
+        </div>
+        <div class="col-md-1"></div>
+        <div class="col-md-5">
+
+
+          <div class="site-section-heading pt-3 mb-4">
+            <h2 class="text-black font-heading-serif">Mulai Usaha</h2>
+          </div>
+          <p>Awalnya kami memulai berjualan mie ayam menggunakan gerobak. Sekarang kami sudah memiliki kedai sendiri.</p>
+          <p>Temui kedai kami di : Jalan Anggrek NO.20, Surabaya.</p>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="site-section py-5 bg-light custom-border-bottom" data-aos="fade">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 order-md-2">
+          <div class="block-16">
+            <figure>
+              <img src="images/mieayamspesial.jpg" alt="Image placeholder" class="img-fluid ">
+            </figure>
+          </div>
+        </div>
+        <div class="col-md-5 mr-auto">
+
+
+          <div class="site-section-heading pt-3 mb-4">
+            <h2 class="text-black font-heading-serif">Menu Andalan Kami</h2>
+          </div>
+          <p>Mie ayam special kami dedikasikan untuk seseorang yang menyukai cita rasa mie ayam klasik.</p>
+          <p>Ini menu yang paling banyak dipesan karena cita rasanya yang enak.</p>
+
+
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+@endsection
